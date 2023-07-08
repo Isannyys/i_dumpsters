2cea7aeab04cdea4e2ba0b0ed4ae89b539e92f9a
Isännyys <114743954+Isannyys@users.noreply.github.com>
2023-07-08 06:08:25 +0300

    Updated sv.lua

--- server/sv.lua
+++ server/sv.lua
@@ -2,23 +2,25 @@
 local ESX = exports['es_extended']:getSharedObject()
 
 ESX.RegisterServerCallback(GetCurrentResourceName(), function(source, cb)
-    local player = ESX.GetPlayerFromId(source)
-    local chance = math.random(1, 100)
+    if safehash == prophash then
+        local player = ESX.GetPlayerFromId(source)
+        local chance = math.random(1, 100)
 
-    if player then
-        for _, item in ipairs(Config.DumpsterLoot) do
-            if chance <= item.Chance then
-                local randomItem = item.name
-                local quantity = math.random(1, item.MaxAmount)
-                local itemLabel = ESX.GetItemLabel(randomItem)
+        if player then
+            for _, item in ipairs(Config.DumpsterLoot) do
+                if chance <= item.Chance then
+                    local randomItem = item.name
+                    local quantity = math.random(1, item.MaxAmount)
+                    local itemLabel = ESX.GetItemLabel(randomItem)
 
-                if player.canCarryItem(randomItem, quantity) then
-                    player.addInventoryItem(randomItem, quantity)
-                    cb(true, itemLabel, quantity)
-                else
-                    cb(false)
+                    if player.canCarryItem(randomItem, quantity) then
+                        player.addInventoryItem(randomItem, quantity)
+                        cb(true, itemLabel, quantity)
+                    else
+                        cb(false)
+                    end
+                    break 
                 end
-                break 
             end
         end
     end
